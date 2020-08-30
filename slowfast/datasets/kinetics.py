1d060d7d3445616f95dce26f97f01b7310e53d26
bqhuyy <bqhuy@apcs.vn>
2020-08-30 16:49:38 +0700

    output pred

--- slowfast/datasets/kinetics.py
+++ slowfast/datasets/kinetics.py
@@ -241,7 +241,7 @@ class Kinetics(torch.utils.data.Dataset):
 
             label = self._labels[index]
             frames = utils.pack_pathway_output(self.cfg, frames)
-            return frames, label, index, {}
+            return frames, label, index, {}, '/'.join(self._path_to_videos[index].split('/')[-2:])
         else:
             raise RuntimeError(
                 "Failed to fetch video after {} retries.".format(
